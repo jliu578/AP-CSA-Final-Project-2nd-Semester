bc61e6bc63d7db799edc5ccb826c48b9abf9a916
jliu578 <jliu578@students.fairmontschools.com>
2025-04-28 11:20:55 -0700

    1.4

--- PathFinding.java
+++ PathFinding.java
@@ -11,29 +11,54 @@ public class PathFinding {
             {1,1,1,1,1,1,1,1,1,1,1}
     };
 
+    static int[][] B = {
+            {0,0,0,0,0,0,0,0,0,1,0},
+            {0,0,0,0,0,0,0,0,0,1,0},
+            {0,0,0,0,0,0,0,0,1,1,0},
+            {0,0,0,0,0,0,0,0,1,0,0},
+            {0,0,0,0,0,0,1,1,1,0,0},
+            {0,0,0,0,0,0,1,0,0,0,0},
+            {1,1,1,1,1,1,1,0,0,0,0},
+    };
+
     static ArrayList<String> answerList = new ArrayList<>();
     static boolean[][] visited = new boolean[A.length][A[0].length];
 
-    private static void dfs(int row, int col) {
-        if (row < 0 || row >= A.length || col < 0 || col >= A[0].length) return;
-        if (A[row][col] != 1 || visited [row][col]) return;
+    private static void dfs(int[][] matrix, int row, int col) {
+        if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length) return;
+        if (matrix[row][col] != 1 || visited[row][col]) return;
 
         visited[row][col] = true;
-        answerList.add("A[" + row + "][" + col + "]");
+        answerList.add("[" + row + "][" + col + "]");
 
-        dfs(row - 1, col);
-        dfs(row + 1, col);
-        dfs(row, col - 1);
-        dfs(row, col + 1);
+        dfs(matrix, row - 1, col);
+        dfs(matrix, row + 1, col);
+        dfs(matrix, row, col - 1);
+        dfs(matrix, row, col + 1);
     }
 
     public static void main(String[] args) {
+
         visited = new boolean[A.length][A[0].length];
 
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[0].length; j++) {
                 if (A[i][j] == 1) {
-                    dfs(i, j);
+                    dfs(A, i, j);
+                    System.out.println(answerList);
+                    answerList.clear();
+                    break;
+                }
+            }
+        }
+
+        visited = new boolean[B.length][B[0].length];
+
+        for (int i = 0; i < B.length; i++) {
+            for (int j = 0; j < B[0].length; j++) {
+                if (B[i][j] == 1) {
+                    System.out.println("B");
+                    dfs(B, i, j);
                     System.out.println(answerList);
                     return;
                 }
