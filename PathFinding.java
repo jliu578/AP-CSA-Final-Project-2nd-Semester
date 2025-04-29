baa38fbc4f113017f1fc7db1d682a6fc2940dc6e
jliu578 <jliu578@students.fairmontschools.com>
2025-04-29 13:54:37 -0700

    1.4

--- PathFinding.java
+++ PathFinding.java
@@ -22,45 +22,44 @@ public class PathFinding {
     };
 
     static ArrayList<String> answerList = new ArrayList<>();
-    static boolean[][] visited = new boolean[A.length][A[0].length];
+    static boolean[][] visited1 = new boolean[A.length][A[0].length];
+    static boolean[][] visited2 = new boolean[B.length][B[0].length];
 
-    private static void dfs(int[][] matrix, int row, int col) {
+    private static void dfs(int[][] matrix, boolean[][] visited, int row, int col) {
         if (row < 0 || row >= matrix.length || col < 0 || col >= matrix[0].length) return;
         if (matrix[row][col] != 1 || visited[row][col]) return;
 
         visited[row][col] = true;
         answerList.add("[" + row + "][" + col + "]");
 
-        dfs(matrix, row - 1, col);
-        dfs(matrix, row + 1, col);
-        dfs(matrix, row, col - 1);
-        dfs(matrix, row, col + 1);
+        dfs(matrix, visited, row - 1, col);
+        dfs(matrix, visited, row + 1, col);
+        dfs(matrix, visited, row, col - 1);
+        dfs(matrix, visited, row, col + 1);
     }
 
     public static void main(String[] args) {
 
-        visited = new boolean[A.length][A[0].length];
-
+        System.out.println("A");
+        int componentCount = 1;
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A[0].length; j++) {
-                if (A[i][j] == 1) {
-                    dfs(A, i, j);
-                    System.out.println(answerList);
+                if (A[i][j] == 1 && !visited1[i][j]) {
+                    dfs(A, visited1, i, j);
+                    System.out.println("Component " + componentCount++ + ": " + answerList);
                     answerList.clear();
-                    break;
                 }
             }
         }
 
-        visited = new boolean[B.length][B[0].length];
-
+        System.out.println("B");
+        componentCount = 1;
         for (int i = 0; i < B.length; i++) {
             for (int j = 0; j < B[0].length; j++) {
-                if (B[i][j] == 1) {
-                    System.out.println("B");
-                    dfs(B, i, j);
-                    System.out.println(answerList);
-                    return;
+                if (B[i][j] == 1 && !visited2[i][j]) {
+                    dfs(B, visited2, i, j);
+                    System.out.println("Component " + componentCount++ + ": " + answerList);
+                    answerList.clear();
                 }
             }
         }
